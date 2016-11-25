0e5b5f0544c1e87a4718349042f5ff569c7f3244
Imanol Pinto <ipintoc@gmail.com>
2016-11-25 23:50:14 +0100

    Second iteration over python pangram exercise

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,6 +1,6 @@
+import string
+
+
 def is_pangram(sentence):
-    sorted_ascii_codes = list(set([ord(letter) for letter in sentence.upper()]))
-    try:
-        return sorted_ascii_codes.index(ord('Z')) - sorted_ascii_codes.index(ord('A')) == ord('Z') - ord('A')
-    except ValueError:
-        return False
+    alphabet = string.ascii_lowercase
+    return all(letter in sentence.lower() for letter in alphabet)
