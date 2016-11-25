725ef234a0087483b45381da2be6bf5909b65ecd
Imanol Pinto <ipintoc@gmail.com>
2016-11-25 16:44:14 +0100

    Add my own algorithm for leap years

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,5 +1,6 @@
-import calendar
 
 
 def is_leap_year(year):
-    return calendar.isleap(year)
+    # Reminder -> PEP 8: do not assign a lambda expression, use a def
+    def is_divisible_by(denominator): return year % denominator == 0
+    return is_divisible_by(4) and (not is_divisible_by(100) or is_divisible_by(400))
