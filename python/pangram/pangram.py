49c2ec85551d21822ce4cca37fe5a2168554ced9
Imanol Pinto <ipintoc@gmail.com>
2016-11-26 18:03:10 +0100

    Add python pangram iteration

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -2,5 +2,4 @@ import string
 
 
 def is_pangram(sentence):
-    alphabet = string.ascii_lowercase
-    return all(letter in sentence.lower() for letter in alphabet)
+    return not set(string.ascii_lowercase) - set(sentence.lower())
